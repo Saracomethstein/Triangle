40a31f0a1a10e9d89ad7e86f1761c17dad85b10b
Saracomethstein <adel.sevastianov@yandex.ru>
2023-09-11 19:19:09 +0300

    Create first task

---
+++ TriangleDefinition/Program.cs
@@ -0,0 +1,128 @@
+﻿using System;
+
+namespace TriangleDefinition
+{
+    class Program
+    {
+        static void Main()
+        {
+            InputSide();
+            Console.ReadLine();
+        }
+
+        static void InputSide()
+        {
+            PrintMsg("Введите длины сторон треугольника.");
+            double side1, side2, side3;
+
+            #region Input Sides
+            while (true)
+            {
+                PrintMsg("Введите первую сторону: ");
+                if (double.TryParse(Console.ReadLine(), out side1) && IsValidSide(side1))
+                {
+                    break;
+                }
+                else
+                {
+                    ErrorPrint("[Error]: Диапазон чисел находиться от 0 до 99.");
+                }
+            }
+
+            while (true)
+            {
+                PrintMsg("Введите вторую сторону: ");
+                if (double.TryParse(Console.ReadLine(), out side2) && IsValidSide(side2))
+                {
+                    break;
+                }
+                else
+                {
+                    ErrorPrint("[Error]: Диапазон чисел находиться от 0 до 99.");
+                }
+            }
+
+            while (true)
+            {
+                PrintMsg("Введите третью сторону: ");
+                if (double.TryParse(Console.ReadLine(), out side3) && IsValidSide(side3))
+                {
+                    break;
+                }
+                else
+                {
+                    ErrorPrint("[Error]: Диапазон чисел находиться от 0 до 99.");
+                }
+            }
+            #endregion
+
+            #region IsTriangle?
+            if (IsTriangle(side1, side2, side3))
+            {
+                string triangleType = GetTriangleType(side1, side2, side3);
+                double area = CalculateTriangleArea(side1, side2, side3);
+                PrintResult($"Треугольник {triangleType} с площадью {area:F2}");
+            }
+            else
+            {
+                ErrorPrint("Треугольникa с такими сторонами не существует.");
+            }
+            #endregion
+        }
+
+        static bool IsValidSide(double side)
+        {
+            return side > 0 && side <= 99;
+        }
+
+        static bool IsTriangle(double side1, double side2, double side3)
+        {
+            return side1 + side2 > side3 && side1 + side3 > side2 && side2 + side3 > side1;
+        }
+
+        static string GetTriangleType(double side1, double side2, double side3)
+        {
+            if (side1 == side2 && side2 == side3)
+            {
+                return "равносторонний";
+            }
+            else if (side1 == side2 || side1 == side3 || side2 == side3)
+            {
+                return "равнобедренный";
+            }
+            else
+            {
+                return "разносторонний";
+            }
+        }
+
+        static double CalculateTriangleArea(double side1, double side2, double side3)
+        {
+            double P = (side1 + side2 + side3) / 2;
+            return Math.Sqrt(P * (P - side1) * (P - side2) * (P - side3));
+        }
+
+        #region Print message
+        static void ErrorPrint(string msg)
+        {
+            Console.ForegroundColor = ConsoleColor.Red;
+            Console.WriteLine(msg);
+            Console.ResetColor();
+        }
+
+        static void PrintResult(string msg)
+        {
+            Console.ForegroundColor = ConsoleColor.Green;
+            Console.WriteLine(msg);
+            Console.ResetColor();
+        }
+
+        static void PrintMsg(string msg)
+        {
+            Console.ForegroundColor = ConsoleColor.Yellow;
+            Console.WriteLine(msg);
+            Console.ResetColor();
+        }
+        #endregion
+    }
+}
